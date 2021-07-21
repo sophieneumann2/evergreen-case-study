5becd9f1cb39c0146f277737a1e2291cdfc55463
Sophie Neumann <sophienewman2401@yahoo.com>
2021-07-21 19:55:11 +0200

    hide/show divs, improved layout

---
+++ main.js
@@ -0,0 +1,60 @@
+const startScreenElement = document.getElementById('div-start');
+const configureAgeElement = document.getElementById('div-birth');
+const configureWorkElement = document.getElementById('div-workSettings');
+const configureRetirementElement = document.getElementById(
+  'div-retirementSettings'
+);
+const furtherSettingsElement = document.getElementById(
+  'div-retirementFurtherSettings'
+);
+const resultSectionElement = document.getElementById('scn-calculatedData');
+const nextButtonsElement = document.querySelectorAll('.btn-next');
+const previousButtonsElement = document.querySelectorAll('.btn-previous');
+
+const configurationElements = [
+  configureAgeElement,
+  configureWorkElement,
+  configureRetirementElement,
+  furtherSettingsElement
+];
+
+const startButtonElement = document.getElementById('btn-start');
+const calculateButtonElement = document.getElementById('btn-doCalculation');
+
+function displayDiv(divToDisplay) {
+  const divThatShouldBeDisplayed = configurationElements[divToDisplay];
+  const divsThatShouldBeHidden = Object.values(configurationElements).filter(
+    (div) => div !== divThatShouldBeDisplayed
+  );
+  divThatShouldBeDisplayed.style.display = '';
+
+  for (const div of divsThatShouldBeHidden) {
+    div.style.display = 'none';
+  }
+}
+
+let currentDivDisplayed = 0;
+
+startButtonElement.addEventListener('click', () => {
+  startScreenElement.style.display = 'none';
+  let currentDivDisplayed = 0;
+  displayDiv(currentDivDisplayed);
+});
+
+calculateButtonElement.addEventListener('click', () => {
+  resultSectionElement.style.display = '';
+});
+
+for (let nextButton of nextButtonsElement) {
+  nextButton.addEventListener('click', () => {
+    currentDivDisplayed++;
+    displayDiv(currentDivDisplayed);
+  });
+}
+
+for (let previousButton of previousButtonsElement) {
+  previousButton.addEventListener('click', () => {
+    currentDivDisplayed--;
+    displayDiv(currentDivDisplayed);
+  });
+}
